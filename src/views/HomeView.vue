8843f237ac8b2387b57fe67628e04c34c34c12a3
cristi <dumitriucristian@yahoo.com>
2024-07-20 17:14:14 +0200

    routerlink

---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import Heroe from '@/components/Heroe.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
+</script>
+
+<template>
+  <Heroe />
+  <HomeCards />
+  <JobListings :limit="3" :showButton="true" />
+</template>
